a23f5f5f417bee19f8f3cfb3317a5e35e51d0310
Max Kohler <max@koehler-kn.de>
2020-09-30 21:36:41 +0100

    Develop design

--- theme/functions.php
+++ theme/functions.php
@@ -58,6 +58,10 @@ class WalkativeSite extends Timber\Site
 		add_action('init', array($this, 'register_taxonomies'));
 		add_filter('allowed_block_types', 'walkative_allowed_block_types');
 		add_action('wp_print_styles', 'walkative_remove_unused_scripts');
+		remove_action('wp_head', 'print_emoji_detection_script', 7);
+		remove_action('wp_print_styles', 'print_emoji_styles');
+		remove_action('admin_print_scripts', 'print_emoji_detection_script');
+		remove_action('admin_print_styles', 'print_emoji_styles');
 		parent::__construct();
 	}
 	public function register_post_types()
