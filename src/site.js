3cf46808b2ab4a093e4b47baece4d97ab863fe90
Max Kohler <max@koehler-kn.de>
2020-10-09 16:45:15 +0100

    Develop homepage design

--- src/site.js
+++ src/site.js
@@ -1,4 +1,11 @@
 const { initLine } = require("./initLine");
+const { initTabs } = require("./initTabs");
+
 document.addEventListener("DOMContentLoaded", () => {
-    initLine()
+  initLine();
+  initTabs();
+  document.body.style.setProperty("--y", 0);
+});
+window.addEventListener("scroll", (e) => {
+  document.body.style.setProperty("--y", window.scrollY);
 });
